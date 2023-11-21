9c9527d568f530b41b4a322cc84cea8e8576beaf
Araw <markistan123@gmail.com>
2023-11-22 05:29:37 +0800

    character update
    im updating the character and its script

---
+++ To Gamble Life- Take The Risk or Be DEVOURED/Assets/Scipts/player folder/playermovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public Animator anim;
+    public float movemespeed = 5f;
+    private Vector2 movement;
+    private bool speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
