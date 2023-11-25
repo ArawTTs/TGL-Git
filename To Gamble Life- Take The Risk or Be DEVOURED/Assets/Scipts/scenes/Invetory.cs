3cd64f45b0c76c945a16793ec4dfd6f859d4d4cf
Araw <markistan123@gmail.com>
2023-11-26 00:29:21 +0800

    1

--- To Gamble Life- Take The Risk or Be DEVOURED/Assets/Scipts/scenes/Invetory.cs
+++ To Gamble Life- Take The Risk or Be DEVOURED/Assets/Scipts/scenes/Invetory.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Invetory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
         
+    }
+
+
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
