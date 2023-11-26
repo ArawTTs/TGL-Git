15ed36156c1e103c278b1d3ae90a8909f7e25629
Araw <markistan123@gmail.com>
2023-11-26 22:08:54 +0800

    noice

---
+++ To Gamble Life- Take The Risk or Be DEVOURED/Assets/Scipts/dialogue/dialogue.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class dialogue
+{
+    // Start is called before the first frame update
+    public string[] sentences;
+    public string name;
+}
---
+++ To Gamble Life- Take The Risk or Be DEVOURED/Assets/Scipts/dialogue/dialogueTrigger.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour
+{
+    public dialogue dialogue;
+}
